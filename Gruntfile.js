03729dd1a9b2ea3aafcb77618feba0995a86fffd
Blas Yaselli <k2blas@blass-mbp-4.k2workflow.com>
2015-09-15 14:39:33 -0600

    First Push

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,11 @@ module.exports = function(grunt) {
 
     sass: {
       options: {
-        includePaths: ['bower_components/foundation/scss']
+        includePaths: ['bower_components/foundation/scss', 'bower_components/bootstrap-sass/assets/stylesheets/bootstrap']
       },
       dist: {
         options: {
-          outputStyle: 'compressed',
+          outputStyle: 'uncompressed',
           sourceMap: true,
         },
         files: {
@@ -18,22 +18,37 @@ module.exports = function(grunt) {
     },
 
     watch: {
+      livereload: {
+        options: { livereload: true },
+        files: ['**/*.html'],
+      },
       grunt: {
         options: {
-          reload: true
+          reload: true,
+          livereload:true
         },
         files: ['Gruntfile.js']
       },
 
       sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
+        files: ['scss/**/**/*.scss','bower_components/bootstrap-sass/assets/stylesheets/bootstrap/**/*.scss'],
+        tasks: ['sass'],
+        
+      },
+      css: {
+        files: 'css/*.css',
+        options: {
+          livereload:true,
+
+        }
       }
+      
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-serve');
 
   grunt.registerTask('build', ['sass']);
   grunt.registerTask('default', ['build','watch']);
